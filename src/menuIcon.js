564416d0e8d5e78d942506f1640246318ed67d3d
Jeremy Park <jeremypark@192.168.nate.com>
2021-08-04 13:44:05 +0900

    grid Layout fixed

--- src/menuIcon.js
+++ src/menuIcon.js
@@ -39,44 +39,44 @@ function MenuIcon(props) {
     return (
         <div className={"w-full h-full bg-white " + (nowPage!=='menu'?' right-full fixed ':null)}>
             <div className="container">
-                <div className="pt-3 grid grid-cols-4 justify-items-center justify-self-center">
-                    <div className="flex flex-col iconOpenAni">
+                <div className="pt-3 grid grid-cols-4 justify-items-center">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('nearStop')}}><i className="fas fa-map-marker-alt fa-lg"></i></button>
                         <div className="iconName w-20">주변정류장</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('timeTable')}}><i className="far fa-clock fa-lg"></i></button>
                         <div className="iconName w-20">운행시간표</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('innerShuttle')}}><i className="fas fa-spinner fa-lg"></i></button>
                         <div className="iconName w-20">사내셔틀</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('favorate')}}><i className="far fa-star fa-lg"></i></button>
                         <div className="iconName w-20">즐겨찾기</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('voc')}}><i className="far fa-question-circle fa-lg"></i></button>
                         <div className="iconName w-20">문의사항</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('notice')}}><i className="fas fa-volume-up fa-lg"></i></button>
                         <div className="iconName w-20">공지사항</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('sue')}}><i className="far fa-question-circle fa-lg"></i></button>
                         <div className="iconName w-20">신고</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('contact')}}><i className="far fa-address-book fa-lg"></i></button>
-                        <div className="iconName w-20 overflow-visible">담당자 연락처</div>
+                        <div className="iconName w-22">담당자 연락처</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('newRoute')}}><i className="far fa-question-circle fa-lg"></i></button>
                         <div className="iconName w-20">노선 신설</div>
                     </div>
-                    <div className="flex flex-col iconOpenAni">
+                    <div className="iconOpenAni gridLayout">
                         <button className="iconStyle w-14 h-14 font-bold" onClick={()=>{doClickIcon('satisfy')}}><i className="far fa-smile fa-lg"></i></button>
                         <div className="iconName w-20">만족도 조사</div>
                     </div>
